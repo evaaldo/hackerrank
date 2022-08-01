4ada7be173a028ecd254c8c36a2c91016b0403b6
evaaldo <110418142+evaaldo@users.noreply.github.com>
2022-08-01 14:10:58 -0500

    Add files via upload

---
+++ plusminus.js
@@ -0,0 +1,25 @@
+function plusMinus(arr) {
+    let posNum = 0;
+    let negNum = 0;
+    let zeroNum = 0;
+
+    arr.forEach((arrElement) => {
+        if(arrElement > 0) {
+            posNum++
+        } else if(arrElement < 0) {
+            negNum++
+        } else {
+            zeroNum++
+        }
+    })
+
+    let ratioPositive = (posNum / arr.length).toPrecision(6);
+    let ratioNegative = (negNum / arr.length).toPrecision(6);
+    let ratioZero = (zeroNum / arr.length).toPrecision(6);
+
+    console.log(ratioPositive);
+    console.log(ratioNegative);
+    console.log(ratioZero);
+}
+
+plusMinus([1,1,0,-1,-1]);
