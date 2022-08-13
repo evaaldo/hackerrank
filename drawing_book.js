10b7313f3edf38af5d84e7750f1fce1e9a8257a8
Evaldo Rodrigues <110418142+evaaldo@users.noreply.github.com>
2022-08-12 23:58:32 -0300

    Add files via upload

---
+++ drawing_book.js
@@ -0,0 +1,7 @@
+function pageCount(n, p) {
+    let frontFlip = Math.floor(p/2);
+    let backFlip = Math.floor((n/2) - frontFlip)
+
+    let result = Math.min(frontFlip, backFlip);
+    return result;
+}
